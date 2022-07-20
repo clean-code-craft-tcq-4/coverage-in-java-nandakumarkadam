6bfe9f896c9c191eeac95fa06cd2654d9a371dd4
Kadam Nandakumar (MS/ETB73-PS) <nkd7kor@bosch.com>
2022-07-20 14:25:49 +0530

    Updated test case

--- src/test/java/typewiseAlert/TypewiseAlertTest.java
+++ src/test/java/typewiseAlert/TypewiseAlertTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import temparatureBreachType.TemparetureBreach;
 import temparatureBreachTypeImpl.TemparetureBreachImpl;
+import typewiseAlert.Constants.AlertTarget;
 import typewiseAlert.Constants.BreachType;
 import typewiseAlert.Constants.CoolingType;
 
@@ -14,9 +15,17 @@ public class TypewiseAlertTest {
 
 	@Test
 	public void infersBreachAsPerLimits() {
-		assertTrue(tempBreach.inferBreach(12, 20, 30) == BreachType.TOO_LOW);
+		assertTrue("test", tempBreach.inferBreach(12, 20, 30) == BreachType.TOO_LOW);
 		assertTrue("test", tempBreach.inferBreach(22, 20, 30) == BreachType.NORMAL);
 		assertTrue("test", tempBreach.inferBreach(32, 20, 30) == BreachType.TOO_HIGH);
 
 	}
+	
+	@Test
+	public void veryfyBreachType() {
+		assertTrue("test", tempBreach.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 55) == BreachType.TOO_HIGH);
+		assertTrue("test", tempBreach.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 15) == BreachType.NORMAL);
+		assertTrue("test", tempBreach.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, -10) == BreachType.TOO_LOW);
+	}
+
 }
